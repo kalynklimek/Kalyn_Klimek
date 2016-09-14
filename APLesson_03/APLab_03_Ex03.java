390de19d948c967ec3a4569e948b3aaa42443684
kalynklimek <kaky000@gmail.com>
2016-09-14 10:25:54 -0700

    Added Files

---
+++ APLesson_03/APLab_03_Ex03.java
@@ -0,0 +1,18 @@
+import java.util.Scanner; //import Statement
+
+public class APLab_03_Ex03
+{
+	public static void main(String[]args)
+	{
+		Scanner keyboard = new Scanner(System.in)
+		
+		System.out.println("What is the height of the rectangle");
+		double num1 = kb.nextDouble();
+		
+		System.out.println("The width?");
+		double num2 = kb.nextDouble();
+		
+		Sy
+		
+		
+	}
