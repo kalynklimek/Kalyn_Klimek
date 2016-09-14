5b3910b6857aae1862b340294770c4e226580a13
kalynklimek <kaky000@gmail.com>
2016-09-14 10:12:36 -0700

    Added Files

--- APLesson_02/Lab_02.java
+++ APLesson_02/Lab_02.java
@@ -5,12 +5,12 @@ public class Lab_02
 		int num1 = 6;
 		int num2 = 7;
 		
-		System.out.println("Six multiplied by seven is" +" "+ num1 * num2);
+		System.out.println("6 multiplied by 7 is" +" "+ num1 * num2);
 	
 		int code = 413;
 		int zip = 10001;
 		
-		System.out.println(code +" "+ "West Broadway, New York" +" "+ zip);
+		System.out.println(code +" "+ "West Broadway\nNew York" +" "+ zip);
 
 		double h = 4.6;
 		double l = 6.7;
