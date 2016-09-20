086f63f14d40b527a900e2662959c18b1dc5cc97
kalynklimek <kaky000@gmail.com>
2016-09-20 09:15:33 -0700

    updated work

--- APLesson_04/AP_Lab_04_receipt.java
+++ APLesson_04/AP_Lab_04_receipt.java
@@ -1,15 +1,51 @@
+//import scanner
+import java.util.Scanner;
 public class AP_Lab_04_receipt
 {
 	public static void main(String[]args)
 	{
-		System.out.printf("%26s  %10.2f", "* Cheese Sandwich ......", 4.50);
-		System.out.printf("\n%26s  %10.2f", "*    French Fries ......", 2.88);
-		System.out.printf("\n%26s  %10.2f", "*            Soda ......", 1.63);
+		//new AP_Lab_04_receipt
+		AP_Lab_04_receipt receipt = new AP_Lab_04_receipt();
+		//new Scanner object
+		Scanner kb = new Scanner(System.in);
 		
-		System.out.printf("\n%26s  %10.2f", "*       Subtotal: ......", 9.01);
-		System.out.printf("\n%26s  %10.2f", "*            Tax: ......", 0.63);
-		System.out.printf("\n%26s  %10.2f", "*          Total: ......", 9.64);
-		System.out.println("\n  ________________________________________");
-		System.out.println("\n   * Thank you for your support *");
+		System.out.println("Please enter item1: ");
+		String item1 = kb.nextLine();
+		System.out.println("Please enter price: ");
+		double price1 = kb.nextDouble();
+		
+		System.out.println("Please enter item2: ");
+		kb.nextLine();
+		String item2 = kb.nextLine();
+		System.out.println("Please enter price: ");
+		double price2 = kb.nextDouble();
+		
+		System.out.println("Please enter item3: ");
+		kb.nextLine();
+		String item3 = kb.nextLine();
+		System.out.println("Please enter price: ");
+		double price3 = kb.nextDouble();
+		
+		double subtotal = (price1 + price2 + price3);
+		
+		double tax = (subtotal/10);
+		
+		double total = (subtotal + tax);
+		
+		System.out.println("<<<<<<<receipt>>>>>>>");
+		receipt.format(item1, price1);
+		receipt.format(item2, price2);
+		receipt.format(item3, price3);
+		receipt.format("Subtotal", subtotal);
+		receipt.format("Tax", tax);
+		receipt.format("Total", total);
+		
+		System.out.println("\n _________________________________________");
+		System.out.println("\n * Thank you for your support*");
+	}
+	
+	public void format(String item, double price)
+	{
+		System.out.printf("\n* %17s ........ %10.2f", item, price);
 	}
 }
