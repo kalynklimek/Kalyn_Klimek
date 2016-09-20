9ad19662aa520203d3d6c72ad8a272f0ca18aec3
kalynklimek <kaky000@gmail.com>
2016-09-20 08:24:21 -0700

    Updated work

--- APLesson_03/APLab_03_Ex03.java
+++ APLesson_03/APLab_03_Ex03.java
@@ -4,15 +4,19 @@ public class APLab_03_Ex03
 {
 	public static void main(String[]args)
 	{
-		Scanner keyboard = new Scanner(System.in)
+		Scanner keyboard = new Scanner(System.in);
 		
-		System.out.println("What is the height of the rectangle");
-		double num1 = kb.nextDouble();
+		System.out.println("What is the length of the rectangle?");
+		double a = keyboard.nextDouble();
 		
 		System.out.println("The width?");
-		double num2 = kb.nextDouble();
+		double b = keyboard.nextDouble();
 		
-		Sy
+		System.out.println("The area of your rectangle is " + a*b);
+		System.out.println("What is the height of the rectangle?");
 		
+		double c = keyboard.nextDouble();
 		
+		System.out.println("The volume of the rectangle is " + a*b*c);
 	}
+}
