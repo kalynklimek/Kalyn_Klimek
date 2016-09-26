5f57a1572ea0bbe7cdbdaa0389aaa1f17c11a7a9
kalynklimek <kaky000@gmail.com>
2016-09-26 09:18:35 -0700

    Added Lesson_04 files

---
+++ APLesson_04/Rectangle.java
@@ -0,0 +1,24 @@
+import jav.util.Scanner;
+public class Rectangle 
+{
+	static String var1 = "Your rectangle is " + " sq ft around.";
+	static int var2;
+	static int var3;
+	static int var4;
+	public static void (main[]args)
+	{
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Please enter the length of the rectangle:");
+		var2 = kb.nextInt();
+		System.out.println("Please enter the width of the rectangle:");
+		var3 = kb.nextInt();
+		System.out.println("Please enter the height of the rectangle:");
+		var4 = kb.nextInt();
+		method1(); 
+	}
+	
+	public static void method1 ()
+	{
+		System.out.println()
+	}
+}
