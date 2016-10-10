88afd820b6e74e8ff92e92693d9e30c44b0ce595
kalynklimek <kaky000@gmail.com>
2016-10-10 09:18:20 -0700

    Added Lesson_05 files

--- APLesson_05/APLab_05_01.java
+++ APLesson_05/APLab_05_01.java
@@ -4,7 +4,23 @@ public class APLab_05_01
 	public static void main(String[]args)
 	{
 		Random rand = new Random();
-		int num = rand.nextInt(6)+1;
+		int player = rand.nextInt(6)+1;
+		int computer = rand.nextInt(6)+1;
 		
+		System.out.println("You rolled a " + player);
+		System.out.println("Computer rolled a " + computer);
+		System.out.println("The winner is " + rollDice(player, computer));
+	}
+	
+	public static String rollDice(int player, int computer)
+	{
+		String winner = "";
+		if (player > computer)
+			winner = "player";
+		
+		if (computer > player)
+			winner = "computer";
+		
+		return winner;
 	}
 }
