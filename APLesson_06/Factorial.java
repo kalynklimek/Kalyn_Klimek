55022608afe4438df92771437549303910a04009
kalynklimek <kaky000@gmail.com>
2016-10-24 09:19:33 -0700

    Added Lesson_06 files

---
+++ APLesson_06/Factorial.java
@@ -0,0 +1,13 @@
+import java.until.Scanner;
+public class Factorial
+{
+	public static void main(String[]args)
+	{
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Please enter a number: ");
+		int v = kb.nextint();
+		int factorial = 1;
+		
+		for(int i = 0)
+	}
+}
