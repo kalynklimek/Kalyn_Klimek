2bccdc8728c7fdb0df0a84d2f5efef90f52bf73b
kalynklimek <kaky000@gmail.com>
2016-11-09 09:20:53 -0800

    added lesson 8

---
+++ APLesson_08/TreeDeg60.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class TreeDeg60
+{
+	public static void main(String[]args)
+	{
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Please enter a word:");
+		System.out.println(tree(kb.next()));	
+	}
+	
+	public static string tree(String word)
+	{
+		while()
+	}
+}
