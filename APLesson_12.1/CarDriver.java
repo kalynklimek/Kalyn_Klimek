59f3f747114a37a5650182509d5b46f947e56518
kalynklimek <kaky000@gmail.com>
2017-01-18 09:19:31 -0800

    Added Lesson 12.1 files

---
+++ APLesson_12.1/CarDriver.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class CarDriver
+{
+	public static void main(String[]args)
+	{
+		new Scanner kb = new Scanner(System.in);
+		System.out.println("Please choose the paint for your car:");
+		
+	}
+}
