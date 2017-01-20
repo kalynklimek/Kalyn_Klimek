e4f2c9a3ae9606bd1a1b66c1ddd7d929e1fe5d6a
kalynklimek <kaky000@gmail.com>
2017-01-20 09:20:17 -0800

    Added Lesson 12.2 files

---
+++ APLesson_12.2/User.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+public class User
+{
+	private String firstName;
+	private String lastName;
+	private String avatar;
+	private int userID;
+	
+	public User(String fN, String lN)
+	{
+		firstName = fN;
+		lastName = lN;
+		avatar = "Undefined";
+		userID = (int)(Math.random()*1000000) + 1;
+	}
+	
+	public User(String fN, String lN, String av)
+	{
+		firstName = fN;
+		lastName = lN;
+		avatar = av;
+		userID = (int)(Math.random()*1000000) + 1;
+	}
+	
+	public String toString()
+	{
+		return "Customer Info...\nFirst Name: " + firstName +
+								"\nLast Name: " + lastName +
+								"\nAvatar: " + avatar +
+								"\nUser ID#: " + userID;
+	}
+	
+	Scanner kb = new Scanner(System.in);
+	public static void main(String[]args)
+	{
+		User pH = new User("Professor", "Handsome");
+		System.out.println(pH);
+		System.out.println();
+		User pH2 = new User("Professor", "Handsome", "profHansizzle");
+		System.out.println(pH2);
+	}
+}
